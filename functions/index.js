a0dc49343c493571c1522571eb97434c15f8741f
Manik Jain <jain.manik@yahoo.co.in>
2020-09-24 03:16:57 +0200

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const {v4 : uuidv4} = require('uuid');
+// const app = express();
+// const main = express();
+
+// //API level configuration
+// main.use('/api/v1', app);
+// main.use(bodyParser.json());
+// main.use(bodyParser.urlencoded({extended : false}));
+// admin.initializeApp(functions.config().firebase);
+
+// //initialise firebase collections
+// const db = admin.firestore();
+// const investors = "investors";
+
+// //enable API to accept requests
+// const webApi = functions.https.onRequest(main);
+// module.exports = {webApi};
+
+console.log('hello');
